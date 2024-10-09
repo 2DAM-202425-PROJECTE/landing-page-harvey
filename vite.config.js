988758d5c22aa15ca03d0435f9bf4dc3b4f737e8
Wharvey123 <hglover@iesebre.com>
2024-10-09 20:24:39 +0200

    Multipagina funcionant

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-// config options
-base: '/landing-page-harvey/',
+    // config options
+    base: '/landing-page-harvey/',
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                preu1: resolve(__dirname, '/preus/preu1.html'),
-                preu2: resolve(__dirname, '/preus/preu2.html'),
-                preu3: resolve(__dirname, '/preus/preu3.html'),
+                preu1: resolve(__dirname, 'preus/preu1.html'),
+                preu2: resolve(__dirname, 'preus/preu2.html'),
+                preu3: resolve(__dirname, 'preus/preu3.html'),
             },
         },
     },
-})
+});
